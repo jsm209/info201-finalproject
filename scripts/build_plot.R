ce0a52234b5e1a0f251be21d77b1522e167ba2a3
j <sylviana@uw.edu>
2019-03-13 00:04:07 -0700

    fix lintr errors

--- scripts/build_plot.R
+++ scripts/build_plot.R
@@ -2,11 +2,19 @@ library(dplyr)
 library(plotly)
 
 build_plot <- function(df, x_axis) {
-  plot_data <- full_join(aggregate(number_of_reviews ~ neighbourhood_group,
-                                   df, mean),
-                         aggregate(as.formula(paste(x_axis, "~ neighbourhood_group")),
-                                   df, mean),
-                         by = "neighbourhood_group")
+  plot_data <- full_join(aggregate(
+    number_of_reviews ~ neighbourhood_group,
+    df, mean
+  ),
+  aggregate(
+    as.formula(paste(
+      x_axis,
+      "~ neighbourhood_group"
+    )),
+    df, mean
+  ),
+  by = "neighbourhood_group"
+  )
 
   x1 <- plot_data[[x_axis]]
   y1 <- plot_data[["number_of_reviews"]]
@@ -18,14 +26,19 @@ build_plot <- function(df, x_axis) {
     xtext <- "Reviews per Month"
   }
 
-  p <- plot_ly(data = plot_data, type = "scatter", mode = "markers",
-               x = x1, y =  y1, text = ~paste0(neighbourhood_group),
-               marker = list(
-                 size = 11,
-                 color = y1)) %>%
-    layout(title = paste("Comparison Between No. of Reviews and", xtext),
-           yaxis = list(title = "No. of Reviews", zeroline = FALSE),
-           xaxis = list(title = xtext, zeroline = FALSE))
+  p <- plot_ly(
+    data = plot_data, type = "scatter", mode = "markers",
+    x = x1, y = y1, text = ~ paste0(neighbourhood_group),
+    marker = list(
+      size = 11,
+      color = y1
+    )
+  ) %>%
+    layout(
+      title = paste("Comparison Between No. of Reviews and", xtext),
+      yaxis = list(title = "No. of Reviews", zeroline = FALSE),
+      xaxis = list(title = xtext, zeroline = FALSE)
+    )
 
   return(p)
 }
